23146bfd2e6224a13c82e3e71d11d199f6feed69
dave <dave@evanlee.co.uk>
2023-08-30 12:46:03 +0000

    some minor tweaks and addition of refund section tbd

---
+++ src/app/refund/page.tsx
@@ -0,0 +1,19 @@
+
+import {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'pawaPay nextjs sandbox - Refund'
+};
+
+export default async function Home() {
+
+
+
+  return (
+      <>
+              <div className="flex flex-col content-center flex-wrap">
+                    Refund - to be done
+              </div>
+      </>
+  )
+}
